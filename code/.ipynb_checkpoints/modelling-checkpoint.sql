33dccdf7993c0c8d6ae9dc42857b8a4a6773ad08
arpit-khatri <arpit.khatri07@gmail.com>
2023-09-15 19:42:25 +0000

    Added the PySpark code for data modeling

--- code/.ipynb_checkpoints/modelling-checkpoint.sql
+++ code/.ipynb_checkpoints/modelling-checkpoint.sql
@@ -1,8 +1,11 @@
 -- Create the database (if it doesn't exist)
 CREATE DATABASE IF NOT EXISTS db_sil_marvel;
 
+-- Use the database
+USE db_sil_marvel;
+
 -- Create a managed table from the Parquet file
-CREATE TABLE IF NOT EXISTS db_sil_marvel.char_stats_day_dly
+CREATE TABLE IF NOT EXISTS char_stats_day_dly
 USING parquet
 OPTIONS (
   path '/home/jovyan/work/digital/target/char_stats.parquet'
