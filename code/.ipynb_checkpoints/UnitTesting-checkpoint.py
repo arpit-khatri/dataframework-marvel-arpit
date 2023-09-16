4199a0fcbb5cb3ebaa1a6a5bf29817a6bdffe29a
arpit-khatri <arpit.khatri07@gmail.com>
2023-09-16 22:37:26 +0000

    Added the script for Data Quality & Unit Testing

--- code/.ipynb_checkpoints/UnitTesting-checkpoint.py
+++ code/.ipynb_checkpoints/UnitTesting-checkpoint.py
@@ -1,31 +1,40 @@
 import unittest
 import yaml
 import datetime
+import argparse
 import pandas as pd
 import os
-
 from pyspark.sql import SparkSession
+
 from pyspark.sql.functions import col, upper
 from pyspark.sql.window import Window
 from pyspark.sql import functions as F
 
+
+
+# Define command-line arguments
+#parser = argparse.ArgumentParser(description="Unit test script for Spark DataFrame schema and data validation")
+#parser.add_argument("--table_name", required=True, help="Path to the target table")
+
 class TestCharStats(unittest.TestCase):
     def setUp(self):
+        # Parse command-line arguments
+        #args = parser.parse_args()
+        #self.table_name = args.table_name
+
         # Loading configuration from config.yaml
         with open("config.yaml", "r") as config_file:
             config = yaml.safe_load(config_file)
-            
-        # Create a Spark session with a custom configuration
-        self.spark = SparkSession.builder \
-            .appName("TestCharStats") \
-            .getOrCreate()
+
+        # Create a Spark session
+        self.spark = SparkSession.builder.appName("TestCharStats").getOrCreate()
+
         # Set the log level to ERROR or FATAL
         self.spark.sparkContext.setLogLevel("ERROR")  # You can change "ERROR" to "FATAL" if needed
-        
+
         # Assign configuration variables
+        self.columns_config = config["columns"]
         self.table_name = config["table_name"]
-        self.primary_key_columns = config["primary_key_columns"]
-        self.threshold_percentage = config["threshold_percentage"]
         self.output_path = config["output_path"]
         self.absolute_table_name =f"{self.output_path}/{self.table_name}"
 
@@ -46,33 +55,19 @@ class TestCharStats(unittest.TestCase):
         # Append the result to the test_results list
         test_results.append(result)
 
-    def test_primary_key(self):
+    def test_column_data_types(self):
         # Loading the current data
-        df = self.spark.read.parquet(self.absolute_table_name)  
+        df = self.spark.read.parquet(self.absolute_table_name)
 
-        # Checking primary key columns to make sure no duplicates
-        num_rows = df.count()
-        num_distinct_rows = df.select(*self.primary_key_columns).distinct().count()
-        if num_rows == num_distinct_rows:
-            self.log_test_result("test_primary_key", "PASS")
-        else:
-            count_duplicate_key = num_rows - num_distinct_rows
-            self.log_test_result("test_primary_key", "FAIL", f"total of {count_duplicate_key} Duplicate primary key values found.")
-
-    def test_count_increase(self):
-        # Loading the today's and previous day data for the comparision of countyour DataFrames for previous and current loads
-        df_previous = self.spark.read.parquet(self.absolute_table_name)
-        df_current = self.spark.read.parquet(self.absolute_table_name)
-
-        # Calculating the count of increased percentage
-        count_previous = df_previous.count()
-        count_current = df_current.count()
-        increase_percentage = (count_current - count_previous) / count_previous * 100
-
-        if increase_percentage >= self.threshold_percentage:
-            self.log_test_result("test_count_increase", "FAIL", f"Count_increase percentage is more than threshold")
-        else:
-            self.log_test_result("test_count_increase", "PASS", "Count_increase percentage is below threshold")
+        # Check if column data types match the configuration
+        for column_config in self.columns_config:
+            column_name = column_config["name"]
+            expected_data_type = column_config["type"]
+            actual_data_type = df.schema[column_name].dataType.simpleString()
+            if actual_data_type == expected_data_type:
+                self.log_test_result(f"{column_name} data_type", "PASS")
+            else:
+                self.log_test_result(f"{column_name} data_type", "FAIL", f"Expected {expected_data_type}, but got {actual_data_type}")
 
 if __name__ == '__main__':
     # Creating an empty list to store test results
@@ -87,34 +82,33 @@ if __name__ == '__main__':
     # Run each test and log the results
     for test_case in test_suite:
         test_result = test_runner.run(test_case)
-        
-   # Create a Spark session
-    spark = SparkSession.builder.appName("DataQuality").getOrCreate()
-    
-    schema = ["Table Name", "Status", "Test Type", "Date"]
-    # Converting the test results to spark dataframe
+
+    # Converting the test results to write into a file
     results_df = pd.DataFrame(test_results)
-    results_df.to_csv("DataQuality.csv", index=False)
-    # Write the individual Data Quality results to a CSV file
-    data_quality_df = spark.createDataFrame(results_df, schema=schema) 
+    results_df.to_csv("UnitTesting.csv", index=False)
+
+    # Create a Spark session and converting the test results to spark dataframe
+    spark = SparkSession.builder.appName("UnitTesting").getOrCreate()
+    #schema = ["Table Name", "Status", "Test Type", "Date"]
+
+    UnitTest_df = spark.createDataFrame(results_df)
     
     
      # Create a summary DataFrame to store load status
-    load_status_df = data_quality_df.groupby("Table Name")\
-        .agg(F.max("Status").alias("Status"), F.lit("Data Quality Checks").alias("Test Type"), F.current_date().alias("Date"))
-
+    load_status_df = UnitTest_df.groupby("Table Name")\
+        .agg(F.max("Status").alias("Status"), F.lit("UnitTesting").alias("Test Type"), F.current_date().alias("Date"))
     # Check if any row in the test results DataFrame has "FAIL" in the "Status" column
     if "FAIL" in load_status_df.select("Status").distinct().rdd.map(lambda x: x[0].upper()).collect():
         # Set the status in the summary DataFrame to "FAIL"
         load_status_df = load_status_df.withColumn("Status", F.lit("FAIL"))
-
+    
     # Define the file path for the load status CSV
     csv_file_path = "load_status"
+    load_status_df.show
     
     # Check if the file already exists
     #if os.path.isfile(f"work/digital/logs/{csv_file_path}.csv"):
     if os.path.isfile(f"{csv_file_path}.csv"):
-        print(f"{csv_file_path}.csv")
         ## Read the existing load status CSV directly
         existing_load_status_df = spark.read.csv(f"{csv_file_path}.csv", header=True, inferSchema=True)
         final_load_status_df = existing_load_status_df.union(load_status_df).distinct()
@@ -125,4 +119,3 @@ if __name__ == '__main__':
         final_load_status_df = load_status_df
         final_load_status_dfp=final_load_status_df.toPandas()
         final_load_status_dfp.to_csv("load_status.csv", mode='a', index=False, header=not os.path.exists("load_status.csv"))
-    
