07d309a7f88c930cb628c524b7bc6c2ab940eecd
arpit-khatri <arpit.khatri07@gmail.com>
2023-09-17 14:58:13 +0000

    Added the Logging process for each job.

--- code/DataQuality.py
+++ code/DataQuality.py
@@ -3,36 +3,46 @@ import yaml
 import datetime
 import pandas as pd
 import os
-
+import logging
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, upper
-from pyspark.sql.window import Window
 from pyspark.sql import functions as F
 
+# Function to initialize the logger with the date-appended log file name
+def init_logger(log_path):
+    os.makedirs(log_path, exist_ok=True)
+    log_file_name = f"DataQuality_{datetime.datetime.now().strftime('%Y%m%d')}.log"
+    logging.basicConfig(
+        filename=os.path.join(log_path, log_file_name),
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
 class TestCharStats(unittest.TestCase):
-    def setUp(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Loading configuration from config.yaml
         with open("config.yaml", "r") as config_file:
             config = yaml.safe_load(config_file)
-            
-        # Create a Spark session with a custom configuration
-        self.spark = SparkSession.builder \
-            .appName("TestCharStats") \
-            .getOrCreate()
+
+        # Create a Spark session
+        self.spark = SparkSession.builder.appName("TestCharStats").getOrCreate()
+
         # Set the log level to ERROR or FATAL
         self.spark.sparkContext.setLogLevel("ERROR")  # You can change "ERROR" to "FATAL" if needed
-        
+
         # Assign configuration variables
         self.table_name = config["table_name"]
         self.primary_key_columns = config["primary_key_columns"]
         self.threshold_percentage = config["threshold_percentage"]
         self.output_path = config["output_path"]
-        self.absolute_table_name =f"{self.output_path}/{self.table_name}"
+        self.log_path = config["monitoring_path"]
+        self.logging_path = config["log_path"]
+        self.absolute_table_name = f"{self.output_path}/{self.table_name}"
+        
+        # Initialize logger
+        init_logger(self.logging_path)
 
-    def tearDown(self):
-        # Stop the Spark session
-        self.spark.stop()
-
+    # Function to log test results
     def log_test_result(self, test_name, status, reason=""):
         timestamp = datetime.datetime.now()
         # Creating a dictionary for the test result
@@ -47,32 +57,106 @@ class TestCharStats(unittest.TestCase):
         test_results.append(result)
 
     def test_primary_key(self):
-        # Loading the current data
-        df = self.spark.read.parquet(self.absolute_table_name)  
+        try:
+            # Log the test start
+            logging.info("Starting the process to to test primary_key duplicate check.")
 
-        # Checking primary key columns to make sure no duplicates
-        num_rows = df.count()
-        num_distinct_rows = df.select(*self.primary_key_columns).distinct().count()
-        if num_rows == num_distinct_rows:
-            self.log_test_result("test_primary_key", "PASS")
-        else:
-            count_duplicate_key = num_rows - num_distinct_rows
-            self.log_test_result("test_primary_key", "FAIL", f"total of {count_duplicate_key} Duplicate primary key values found.")
+            # Loading the current data
+            df = self.spark.read.parquet(self.absolute_table_name)
+            logging.info(f"Loaded today's data from {self.table_name}.")
+
+            # Checking primary key columns to make sure no duplicates
+            num_rows = df.count()
+            num_distinct_rows = df.select(*self.primary_key_columns).distinct().count()
+            if num_rows == num_distinct_rows:
+                self.log_test_result("test_primary_key", "PASS")
+                logging.info("Duplicate Primary key test passed.")
+            else:
+                count_duplicate_key = num_rows - num_distinct_rows
+                self.log_test_result("test_primary_key", "FAIL", f"Total of {count_duplicate_key} duplicate primary key values found")
+                logging.warning(f"Primary key test failed: Total of {count_duplicate_key} duplicate primary key values found")
+
+            # Log the test end
+            logging.info("Completed the Primary key duplicate test.")
+        except Exception as e:
+            logging.error(f"Error in test_primary_key: {str(e)}")
 
     def test_count_increase(self):
-        # Loading the today's and previous day data for the comparision of countyour DataFrames for previous and current loads
-        df_previous = self.spark.read.parquet(self.absolute_table_name)
-        df_current = self.spark.read.parquet(self.absolute_table_name)
+        try:
+            # Log the test start
+            logging.info("Starting the data quality check to monitor count_increase in today's load.")
 
-        # Calculating the count of increased percentage
-        count_previous = df_previous.count()
-        count_current = df_current.count()
-        increase_percentage = (count_current - count_previous) / count_previous * 100
+            # Loading the today's and previous day data for the comparison of count
+            df_previous = self.spark.read.parquet(self.absolute_table_name)
+            df_current = self.spark.read.parquet(self.absolute_table_name)
+            logging.info(f"Loaded data from yesterday data from table {self.table_name} for count comparison.")
 
-        if increase_percentage >= self.threshold_percentage:
-            self.log_test_result("test_count_increase", "FAIL", f"Count_increase percentage is more than threshold")
-        else:
-            self.log_test_result("test_count_increase", "PASS", "Count_increase percentage is below threshold")
+            # Calculating the count increase percentage
+            count_previous = df_previous.count()
+            count_current = df_current.count()
+            increase_percentage = (count_current - count_previous) / count_previous * 100
+
+            if increase_percentage >= self.threshold_percentage:
+                self.log_test_result("test_count_increase", "FAIL", f"Count increase percentage ({increase_percentage}%) is more than threshold ({self.threshold_percentage}%)")
+                logging.warning(f"Count increase test failed: Count increase percentage ({increase_percentage}%) is more than threshold ({self.threshold_percentage}%)")
+            else:
+                self.log_test_result("test_count_increase", "PASS", f"Count increase percentage  is below threshold ({self.threshold_percentage}%)")
+                logging.info(f"Count increase test passed: Count increase percentage is below threshold ({self.threshold_percentage}%)")
+
+            # Log the test end
+            logging.info("Completed the data check for count_increase in today's load.")
+        except Exception as e:
+            logging.error(f"Error in data count_increase process: {str(e)}")
+
+    def daily_log_status(self, results_df):
+        try:
+            # Log the daily status start
+            logging.info("Starting the function to log the job satsus in daily_log_status table.")
+
+            # Write the individual Data Quality results to a CSV file
+            results_df.to_csv(f"{self.log_path}/DataQuality.csv", index=False)
+            logging.info(f"Saved test results of  DataQuality table.")
+
+            spark = SparkSession.builder.appName("DataQuality").getOrCreate()
+            schema = ["Table Name", "Test Type", "Status", "Reason", "Date"]
+            data_quality_df = spark.createDataFrame(results_df, schema=schema)
+            data_quality_df.show(truncate=False)
+            logging.info("Displayed Data Quality results.")
+
+            # Creating a summary DataFrame to store load status
+            load_status_df = data_quality_df.groupby("Table Name") \
+                .agg(F.max("Status").alias("Status"), F.lit("DataQuality").alias("Test Type"), F.current_date().alias("Date"))
+
+            # Checking if any row in the test results DataFrame has "FAIL" in the "Status" column
+            if "FAIL" in load_status_df.select("Status").distinct().rdd.map(lambda x: x[0].upper()).collect():
+                # Set the status in the summary DataFrame to "FAIL"
+                load_status_df = load_status_df.withColumn("Status", F.lit("FAIL"))
+                logging.warning("Load status set to FAIL due to Data Quality test failures.")
+            else:
+                logging.info("Load status remains as PASS.")
+
+            # Define the file path for the load status CSV
+            csv_file_path = "load_status"
+
+            # Check if the log file already exists
+            if os.path.isfile(f"{self.log_path}/{csv_file_path}.csv"):
+                # Reading the existing load status CSV directly
+                existing_load_status_df = spark.read.csv(f"{self.log_path}/{csv_file_path}.csv", header=True, inferSchema=True)
+                final_load_status_df = existing_load_status_df.union(load_status_df).distinct()
+                final_load_status_dfp = final_load_status_df.toPandas()
+                final_load_status_dfp.to_csv(f"{self.log_path}/load_status.csv", index=False)
+                logging.info(f"Appended load status to existing load_status table.")
+            else:
+                # if the file doesn't exist, creating it with the current load status
+                final_load_status_df = load_status_df
+                final_load_status_dfp = final_load_status_df.toPandas()
+                final_load_status_dfp.to_csv(f"{self.log_path}/load_status.csv", mode='a', index=False, header=not os.path.exists(f"{self.log_path}/load_status.csv"))
+                logging.info(f"Created load_status with current load status")
+
+            # Log the daily status end
+            logging.info("Completed the daily_log_status")
+        except Exception as e:
+            logging.error(f"Error in daily_log_status: {str(e)}")
 
 if __name__ == '__main__':
     # Creating an empty list to store test results
@@ -88,44 +172,10 @@ if __name__ == '__main__':
     for test_case in test_suite:
         test_result = test_runner.run(test_case)
         
-   # Create a Spark session
-    spark = SparkSession.builder.appName("DataQuality").getOrCreate()
-    
-    schema = ["Table Name", "Test Type", "Status", "Reason", "Date"]
-    # Converting the test results to spark dataframe
+     # Converting the test results to write into a file
     results_df = pd.DataFrame(test_results)
-    results_df.to_csv("DataQuality.csv", index=False)
-    # Write the individual Data Quality results to a CSV file
-    data_quality_df = spark.createDataFrame(results_df, schema=schema) 
-    data_quality_df.show()
-    
-     # Create a summary DataFrame to store load status
-    #load_status_df = data_quality_df.groupby("Table Name")\
-     #   .agg(F.max("Status").alias("Status"), F.lit("Data Quality Checks").alias("Test Type"), F.date_sub(F.current_date(), 1).alias("Date"))
-    load_status_df = data_quality_df.groupby("Table Name")\
-        .agg(F.max("Status").alias("Status"), F.lit("Data Quality Checks").alias("Test Type"), F.current_date().alias("Date"))
 
-
-    # Check if any row in the test results DataFrame has "FAIL" in the "Status" column
-    if "FAIL" in load_status_df.select("Status").distinct().rdd.map(lambda x: x[0].upper()).collect():
-        # Set the status in the summary DataFrame to "FAIL"
-        load_status_df = load_status_df.withColumn("Status", F.lit("FAIL"))
-
-    # Define the file path for the load status CSV
-    csv_file_path = "load_status"
-    
-    # Check if the file already exists
-    #if os.path.isfile(f"work/digital/logs/{csv_file_path}.csv"):
-    if os.path.isfile(f"{csv_file_path}.csv"):
-        print(f"{csv_file_path}.csv")
-        ## Read the existing load status CSV directly
-        existing_load_status_df = spark.read.csv(f"{csv_file_path}.csv", header=True, inferSchema=True)
-        final_load_status_df = existing_load_status_df.union(load_status_df).distinct()
-        final_load_status_dfp=final_load_status_df.toPandas()
-        final_load_status_dfp.to_csv("load_status.csv",index=False)
-    else:
-        # The file doesn't exist, create it with the current load status
-        final_load_status_df = load_status_df
-        final_load_status_dfp=final_load_status_df.toPandas()
-        final_load_status_dfp.to_csv("load_status.csv", mode='a', index=False, header=not os.path.exists("load_status.csv"))
-    
+    # Call the daily_log_status function with test_results as an argument
+    test_instance = TestCharStats()
+    test_instance.daily_log_status(results_df)
+    logging.info("Data Quality Job completed successfully.")
--- code/UnitTesting.py
+++ code/UnitTesting.py
@@ -4,24 +4,30 @@ import datetime
 import argparse
 import pandas as pd
 import os
+import logging
 from pyspark.sql import SparkSession
 
 from pyspark.sql.functions import col, upper
 from pyspark.sql.window import Window
 from pyspark.sql import functions as F
 
-
+# Function to initialize the logger with the date-appended log file name
+def init_logger(log_path):
+    os.makedirs(log_path, exist_ok=True)
+    log_file_name = f"UnitTest_{datetime.datetime.now().strftime('%Y%m%d')}.log"
+    logging.basicConfig(
+        filename=os.path.join(log_path, log_file_name),
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
 # Define command-line arguments
-#parser = argparse.ArgumentParser(description="Unit test script for Spark DataFrame schema and data validation")
-#parser.add_argument("--table_name", required=True, help="Path to the target table")
+# parser = argparse.ArgumentParser(description="Unit test script for Spark DataFrame schema and data validation")
+# parser.add_argument("--table_name", required=True, help="Path to the target table")
 
 class TestCharStats(unittest.TestCase):
-    def setUp(self):
-        # Parse command-line arguments
-        #args = parser.parse_args()
-        #self.table_name = args.table_name
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Loading configuration from config.yaml
         with open("config.yaml", "r") as config_file:
             config = yaml.safe_load(config_file)
@@ -36,12 +42,14 @@ class TestCharStats(unittest.TestCase):
         self.columns_config = config["columns"]
         self.table_name = config["table_name"]
         self.output_path = config["output_path"]
-        self.absolute_table_name =f"{self.output_path}/{self.table_name}"
+        self.log_path = config["monitoring_path"]
+        self.logging_path = config["log_path"]
+        self.absolute_table_name = f"{self.output_path}/{self.table_name}"
 
-    def tearDown(self):
-        # Stop the Spark session
-        self.spark.stop()
+        # Initialize logger
+        init_logger(self.logging_path)
 
+    # Function to log test results
     def log_test_result(self, test_name, status, reason=""):
         timestamp = datetime.datetime.now()
         # Creating a dictionary for the test result
@@ -56,23 +64,82 @@ class TestCharStats(unittest.TestCase):
         test_results.append(result)
 
     def test_column_data_types(self):
-        # Loading the current data
-        df = self.spark.read.parquet(self.absolute_table_name)
+        try:
+            # Log the test start
+            logging.info("Starting the Unit Testing to match columns and datatypes.")
 
-        # Check if column data types match the configuration
-        for column_config in self.columns_config:
-            column_name = column_config["name"]
-            expected_data_type = column_config["type"]
-            actual_data_type = df.schema[column_name].dataType.simpleString()
-            if actual_data_type == expected_data_type:
-                self.log_test_result(f"{column_name} data_type", "PASS")
+            # Loading the current data
+            df = self.spark.read.parquet(self.absolute_table_name)
+            logging.info(f"Loaded data from {self.table_name}.")
+
+            # Check if column data types match the configuration
+            for column_config in self.columns_config:
+                column_name = column_config["name"]
+                expected_data_type = column_config["type"]
+                actual_data_type = df.schema[column_name].dataType.simpleString()
+                if actual_data_type == expected_data_type:
+                    self.log_test_result(f"{column_name} data_type", "PASS")
+                else:
+                    self.log_test_result(f"{column_name} data_type", "FAIL", f"Expected {expected_data_type}, but got {actual_data_type}")
+
+            # Log the test end
+            logging.info("Unit Testing to match columns and datatype.")
+        except Exception as e:
+            logging.error(f"Error in test_column_data_types: {str(e)}")
+
+    # Function to log daily status
+    def daily_log_status(self, results_df):
+        try:
+            # Log the daily status start
+            logging.info("Starting the function to log the job satsus in daily_log_status table.")
+
+            # Converting the test results to write into a file
+            results_df.to_csv(f"{self.log_path}/UnitTest.csv", index=False)
+            logging.info(f"Saved test results to UnitTest table.")
+
+            # Creating a Spark session and converting the test results to a Spark DataFrame
+            spark = SparkSession.builder.appName("UnitTest").getOrCreate()
+            UnitTest_df = spark.createDataFrame(results_df)
+            UnitTest_df.show(truncate=False)
+            logging.info("Creating a dataframe which will have daily_log_status.")
+
+            # Creating a summary DataFrame to store load status
+            load_status_df = UnitTest_df.groupby("Table Name") \
+                .agg(F.max("Status").alias("Status"), F.lit("UnitTest").alias("Test Type"), F.current_date().alias("Date"))
+
+            # Checking if any row in the test results DataFrame has "FAIL" in the "Status" column
+            if "FAIL" in load_status_df.select("Status").distinct().rdd.map(lambda x: x[0].upper()).collect():
+                # Set the status in the summary DataFrame to "FAIL"
+                load_status_df = load_status_df.withColumn("Status", F.lit("FAIL"))
+                logging.info("Set load status to FAIL if any of the units test cases failed.")
+
+            # Define the file path for the load status CSV
+            csv_file_path = "load_status"
+
+            # Check if the log file already exists
+            if os.path.isfile(f"{self.log_path}/{csv_file_path}.csv"):
+                # Reading the existing load status CSV directly
+                existing_load_status_df = spark.read.csv(f"{self.log_path}/{csv_file_path}.csv", header=True, inferSchema=True)
+                final_load_status_df = existing_load_status_df.union(load_status_df).distinct()
+                final_load_status_dfp = final_load_status_df.toPandas()
+                final_load_status_dfp.to_csv(f"{self.log_path}/load_status.csv", index=False)
+                logging.info(f"Appended the today's load status to table load_status.")
             else:
-                self.log_test_result(f"{column_name} data_type", "FAIL", f"Expected {expected_data_type}, but got {actual_data_type}")
+                # if the file doesn't exist, creating it with the current load status
+                final_load_status_df = load_status_df
+                final_load_status_dfp = final_load_status_df.toPandas()
+                final_load_status_dfp.to_csv(f"{self.log_path}/load_status.csv", mode='a', index=False, header=not os.path.exists(f"{self.log_path}/load_status.csv"))
+                logging.info(f"Created {self.log_path}/load_status.csv with current load status")
+
+            # Log the daily status end
+            logging.info("Completed updating the daily_log_status for today's job status.")
+        except Exception as e:
+            logging.error(f"Error in daily_log_status: {str(e)}")
 
 if __name__ == '__main__':
     # Creating an empty list to store test results
     test_results = []
-
+    
     # Creating a test suite
     test_suite = unittest.TestLoader().loadTestsFromTestCase(TestCharStats)
 
@@ -85,35 +152,9 @@ if __name__ == '__main__':
 
     # Converting the test results to write into a file
     results_df = pd.DataFrame(test_results)
-    results_df.to_csv("UnitTesting.csv", index=False)
 
-    # Create a Spark session and converting the test results to spark dataframe
-    spark = SparkSession.builder.appName("UnitTesting").getOrCreate()
-    #schema = ["Table Name", "Status", "Test Type", "Date"]
-
-    UnitTest_df = spark.createDataFrame(results_df)
-    UnitTest_df.show(truncate=False)
-
-     # Create a summary DataFrame to store load status
-    load_status_df = UnitTest_df.groupby("Table Name")\
-        .agg(F.max("Status").alias("Status"), F.lit("UnitTesting").alias("Test Type"), F.current_date().alias("Date"))
-    # Check if any row in the test results DataFrame has "FAIL" in the "Status" column
-    if "FAIL" in load_status_df.select("Status").distinct().rdd.map(lambda x: x[0].upper()).collect():
-        # Set the status in the summary DataFrame to "FAIL"
-        load_status_df = load_status_df.withColumn("Status", F.lit("FAIL"))
-    
-    # Define the file path for the load status CSV
-    csv_file_path = "load_status"
-    # Check if the file already exists
-    #if os.path.isfile(f"work/digital/logs/{csv_file_path}.csv"):
-    if os.path.isfile(f"{csv_file_path}.csv"):
-        ## Read the existing load status CSV directly
-        existing_load_status_df = spark.read.csv(f"{csv_file_path}.csv", header=True, inferSchema=True)
-        final_load_status_df = existing_load_status_df.union(load_status_df).distinct()
-        final_load_status_dfp=final_load_status_df.toPandas()
-        final_load_status_dfp.to_csv("load_status.csv",index=False)
-    else:
-        # The file doesn't exist, create it with the current load status
-        final_load_status_df = load_status_df
-        final_load_status_dfp=final_load_status_df.toPandas()
-        final_load_status_dfp.to_csv("load_status.csv", mode='a', index=False, header=not os.path.exists("load_status.csv"))
+    # Call the daily_log_status function with test_results as an argument
+    test_instance = TestCharStats()
+    test_instance.daily_log_status(results_df)
+    print("UnitTesting Job completed successfully.")
+    logging.info("UnitTesting Job completed successfully.")
